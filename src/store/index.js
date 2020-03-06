23063ccb0ded5d9e9ec08c785c4d8159dc4b84ad
jlozan64 <janeth2.jl@gmail.com>
2020-03-05 20:47:37 -0600

    complete

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,42 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { search } from '../helpers/axios'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+    heroes: [],
+    searchItem: '',
+    loading: false,
+    notFound: false,
+  },
+  getters: {
+    getHeroes: state => state.heroes,
+    getHero: state => id => state.heroes.find(hero => hero.id === id),
+    getSearchItem: state => state.searchItem,
+    isLoading: state => state.loading,
+    getNotFound: state => state.notFound,
+  },
+  mutations: {
+    settingHeroes: (state, heroes) => (state.heroes = heroes),
+    settingSearchItem: (state, seatchItem) => (state.searchItem = seatchItem),
+    changingLoading: (state, status) => (state.loading = status),
+    changingNotFound: (state, status) => (state.notFound = status),
+  },
+  actions: {
+    setHeros: ({ commit }, data) => commit('settingHeroes', data),
+    setSearchItem: async ({ commit, dispatch }, searchItem) => {
+      commit('settingSearchItem', searchItem)
+      dispatch('changeNotFound', false)
+      commit('changingLoading', true)
+      const heroes = await search(searchItem)
+      commit('changingLoading', false)
+      heroes.data?.results
+        ? dispatch('setHeros', heroes.data.results)
+        : dispatch('changeNotFound', true) && dispatch('setHeros', [])
+    },
+    changeLoading: ({ commit }, status) => commit('changingLoading', status),
+    changeNotFound: ({ commit }, status) => commit('changingNotFound', status),
+  },
+})
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <label class="block px-2 pt-1 md:px-16 md:pt-8">
+      <input
+        :value="searchItem"
+        type="text"
+        class="form-input mt-1 block w-full"
+        placeholder="Search a Superhero, example: Thor"
+        @change="setSearchItem"
+      />
+    </label>
+    <div class="flex flex-col flex-wrap justify-center items-center">
+      <PulseLoader v-if="loading" class="mt-4" color="#38B2AC" size="2em" />
+      <h1
+        v-if="searchItem"
+        class="w-full text-center px-4 md:px-16 text-teal-200 font-semibold text-3xl md:text-4xl lg:text-5xl xl:text-6xl"
+      >
+        <span class="font-hairline">Showing results for</span> "{{
+          searchItem
+        }}"
+      </h1>
+      <div v-if="notFound">
+        <img :src="notFoundImage" alt="not found" />
+      </div>
+      <div v-if="searchItem && heroes.length > 0" class="w-full flex flex-wrap">
+        <Card
+          class="sm:w-1/2 lg:w-1/3 xl:w-1/4 p-2"
+          v-for="hero in heroes"
+          :key="hero.id"
+          :hero="hero"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+import Card from '@/components/Card.vue'
+import notFoundImage from '../assets/notFound.gif'
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
-  }
-};
+    Card,
+    PulseLoader,
+  },
+  data() {
+    return {
+      notFoundImage,
+    }
+  },
+  computed: {
+    searchItem() {
+      return this.$store.getters.getSearchItem
+    },
+    heroes() {
+      return this.$store.getters.getHeroes
+    },
+    loading() {
+      return this.$store.getters.isLoading
+    },
+    notFound() {
+      return this.$store.getters.getNotFound
+    },
+  },
+  methods: {
+    setSearchItem(event) {
+      this.$store.dispatch('setSearchItem', event.target.value)
+    },
+  },
+}
 </script>
